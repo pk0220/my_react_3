05add664b0469885668bb24e3f1a810c3da751fd
pk0220 <pratikkhamkar220@gmail.com>
2022-01-05 14:56:16 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MyComponent />
+    </>
   );
 }
 
-export default App;
+function MyComponent() {
+  let username = "Pratik";
+  let email = "pratikkhamkar2220@gmail.com";
+  let id = 20;
+  let list = ["Pune", " Mumbai", " Delhi"];
+  let obj = { id: 20, name: "Rahul" };
+  return (
+    <div>
+      <h1>Learning Interpolation</h1>
+      <h1>
+        {username} {email}
+      </h1>
+      <h1>{list}</h1>
+      <h1>{obj.id}</h1>
+      <h1>{`I am ${obj.name} ${obj.id}`}</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugiat,
+        nisi dolorem laboriosam quibusdam odit, saepe ex deserunt, quis rem nam
+        at sed quo sunt expedita magnam! Veritatis exercitationem officiis nisi
+        et! Illum dicta maxime, veniam placeat iure architecto molestias saepe
+        numquam? Perspiciatis vel tenetur quibusdam architecto ipsa, nisi
+        nostrum.
+      </p>
+    </div>
+  );
+}
