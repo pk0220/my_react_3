d16548f5f7a9633e9d2789ed15247a68bb9dfee0
pk0220 <pratikkhamkar220@gmail.com>
2022-01-05 15:09:39 +0530

    interpolation

--- src/App.js
+++ src/App.js
@@ -7,28 +7,19 @@ export default function App() {
 }
 
 function MyComponent() {
-  let username = "Pratik";
-  let email = "pratikkhamkar2220@gmail.com";
-  let id = 20;
-  let list = ["Pune", " Mumbai", " Delhi"];
-  let obj = { id: 20, name: "Rahul" };
+  const id = 20;
+  let background = "bg-danger";
   return (
     <div>
-      <h1>Learning Interpolation</h1>
-      <h1>
-        {username} {email}
+      <h1>Learning Interpolation {id + 200}</h1>
+      <h1>Learning Interpolation {id + 100}</h1>
+
+      <h1 id="20" className={background}>
+        Interpolation {id}
       </h1>
-      <h1>{list}</h1>
-      <h1>{obj.id}</h1>
-      <h1>{`I am ${obj.name} ${obj.id}`}</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugiat,
-        nisi dolorem laboriosam quibusdam odit, saepe ex deserunt, quis rem nam
-        at sed quo sunt expedita magnam! Veritatis exercitationem officiis nisi
-        et! Illum dicta maxime, veniam placeat iure architecto molestias saepe
-        numquam? Perspiciatis vel tenetur quibusdam architecto ipsa, nisi
-        nostrum.
-      </p>
+      <h1 id={id} className="bg-info">
+        Interpolation {id}
+      </h1>
     </div>
   );
 }
